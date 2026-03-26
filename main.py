b089ff654abba4d9f617563631a7eebed080ad62
a8v0r6 <a8v0r6@protonmail.com>
2026-03-26 22:44:53 +0530

    added sample data json for view api

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
-
+import json
 app = FastAPI()
+
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+    return data
 
 @app.get("/")
 def hello():
-    return {'message' : "Hello world"}
+    return {'message' : "Patient Management System API"}
 
 @app.get("/about")
 def about():
     return {'message' : "Supp !! what do you wanna know? "}
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
