580ddb0bcc29c9be46e79c342b33a11153755c3d
a8v0r6 <a8v0r6@protonmail.com>
2026-03-27 11:43:22 +0530

    query param, sorting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 app = FastAPI()
 
@@ -25,4 +25,17 @@ def patient(patient_id: str = Path(..., description="get patient by patient ID",
     data = load_data()
     if patient_id in data:
         return data[patient_id]
-    return {'error' : 'Patient not found'}
+    raise HTTPException(status_code=404, detail='Patient Not found')
+
+@app.get("/sort")
+def sort_patients(sort_by: str = Query(..., description='Sort on the basis of height, weight or bmi'), order: str = Query('asc', description='sort in asc or desc order')):
+    valid_fields = ['height', 'weight', 'bmi']
+
+    if (sort_by not in valid_fields):
+        raise HTTPException(status_code=400, detail=f'Invalid field, select from {valid_fields}')
+    if (order not in ['asc', 'desc']):
+        raise HTTPException(status_code=400, detail='Invalid order field, select from asc or desc')
+    data = load_data()
+    sort_order = ()
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=(order == 'desc'))
+    return sorted_data
