9842ea4117f2b1392f1bf8bcea65dcb0c66c68ce
a8v0r6 <a8v0r6@protonmail.com>
2026-03-30 11:04:59 +0530

    field validations

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 from fastapi import FastAPI, Path, HTTPException, Query
+from pydantic import BaseModel, Field, EmailStr, AnyUrl, field_validator
+from typing import List, Dict, Optional, Annotated
 import json
 app = FastAPI()
+
+class Patient(BaseModel):
+    name: Annotated[str, Field(max_length=50, title='Name of patient')]
+    age: int = Field(ge=18)
+    # we can use Field for validation like greater than, less than or any lambda
+    weight: float = Field(gt=0)
+    isMarried: bool = False
+    linkedin_url: AnyUrl
+    email: EmailStr
+    # can set default values
+    allergies: Optional[List[str]] = Field(max_length=5)
+    # by default pydantic fields validate to be non empty and we can use Optional from typing for optional fields
+    contact_details: Dict[str, str]
+    
+    @field_validator('email')
+    @classmethod
+    def email_validator(cls, value):
+        
+        valid_domains = ['hdfc.com', 'icici.com']
+        domain_name = value.split('@')[1]
+        
+        if (domain_name not in valid_domains):
+            raise ValueError('Not a valid domain')
+
 
 def load_data():
     with open('patients.json', 'r') as f:
